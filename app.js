a4d1f1963801f4d64c0c394da7d2aab786156384
Manuel-Mantilla <santo2244@gmail.com>
2025-02-02 11:33:05 -0500

    Validación de vacios al usar función agregarAmigo en app.js

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaDeAmigos = []
+
 function agregarAmigo() {
     let nombreAmigo = document.getElementById("amigo").value;
-    console.log(nombreAmigo);
+    if (nombreAmigo.trim() === "") {
+        alert("!Por favor ingrese un nombre valido¡")
+    } else {
+        listaDeAmigos.push(nombreAmigo)
+        console.log(listaDeAmigos)
+    }
 }
